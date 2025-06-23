11f029c01756ae7b9d93f2495ed797631e0ee1b7
Boragadda Srinadh <bsrinadhnaidu@gmail.com>
2025-06-23 16:57:18 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import train_test_split
+
+# dataset 
+df = pd.read_csv("Titanic-Dataset.csv")
+print(df)
+
+# basic info
+print(df.info())
+print(df.isnull().sum())
+
+# dealing with missing values 
+df['Age'].fillna(df['Age'].median() , inplace = True)
+df['Embarked'].fillna(df['Embarked'].mode()[0] , inplace = True)
+df.drop(['Cabin'],axis = 1, inplace = True)
+print(df.isnull().sum())
+
+# car=tegorical data into numerical data
+label_encoder = LabelEncoder()
+df['Sex'] = label_encoder.fit_transform(df['Sex'])
+df['Embarked'] = label_encoder.fit_transform(df['Embarked'])
+
+# standardise and normalizing the data
+features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Embarked']
+X=df[features]
+y = df['Survived']
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.2, random_state=42, stratify=y)
+
+
+# Boxplots fro visualization and IQR for removing the outliers
+numerical_cols = ['Age', 'SibSp', 'Parch', 'Fare']
+for col in numerical_cols:
+    sns.boxplot(x=df[col])
+    plt.title(f'Boxplot of {col}')
+    plt.show()
+    Q1 = df[col].quantile(0.25)
+    Q3 = df[col].quantile(0.75)
+    IQR = Q3 - Q1
+    lower = Q1 - 1.5 * IQR
+    upper = Q3 + 1.5 * IQR
+    df = df[(df[col] >= lower) & (df[col] <= upper)]
